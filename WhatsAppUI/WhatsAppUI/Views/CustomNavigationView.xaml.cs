eec886a3aa16cab83dacff806238cba620030b8e
egbakou <egbakoukodjolaurent@gmail.com>
2019-05-03 06:32:55 +0000

    Custom navigation view added.

---
+++ WhatsAppUI/WhatsAppUI/Views/CustomNavigationView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhatsApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomNavigationView : NavigationPage
+    {
+        public CustomNavigationView() : base()
+        {
+            InitializeComponent();
+        }
+
+        public CustomNavigationView(Page root) : base(root)
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
