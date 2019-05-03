4dc19613c89825944a2e8786238d24e165d523b2
egbakou <egbakoukodjolaurent@gmail.com>
2019-05-03 06:36:02 +0000

    DialogService added (MVVM)

---
+++ WhatsAppUI/WhatsAppUI/Services/Dialog/DialogService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Acr.UserDialogs;
+
+namespace WhatsApp.Services.Dialog
+{
+    public class DialogService : IDialogService
+    {
+        public Task ShowAlertAsync(string message, string title, string buttonLabel)
+        {
+            return UserDialogs.Instance.AlertAsync(message, title, buttonLabel);
+        }
+
+    }
+}
---
+++ WhatsAppUI/WhatsAppUI/Services/Dialog/IDialogService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace WhatsApp.Services.Dialog
+{
+    public interface IDialogService
+    {
+         Task ShowAlertAsync(string message, string title, string buttonLabel);
+    }
+}
