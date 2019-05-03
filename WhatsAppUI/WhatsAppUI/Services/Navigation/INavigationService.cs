64eaa810be5fda4db6cdb1040194329b408d5f6d
egbakou <egbakoukodjolaurent@gmail.com>
2019-05-03 06:38:34 +0000

    NavigationService added(MVVM)

---
+++ WhatsAppUI/WhatsAppUI/Services/Navigation/INavigationService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using WhatsApp.ViewModels;
+
+namespace WhatsApp.Services.Navigation
+{
+    public interface INavigationService
+    {
+        BaseViewModel PreviousPageViewModel { get; }
+
+        Task InitializeAsync();
+
+        Task NavigateToAsync<TViewModel>() where TViewModel : BaseViewModel;
+
+        Task NavigateToAsync<TViewModel>(object parameter) where TViewModel : BaseViewModel;
+
+        Task RemoveLastFromBackStackAsync();
+
+        Task RemoveBackStackAsync();
+    }
+}
---
+++ WhatsAppUI/WhatsAppUI/Services/Navigation/NavigationService.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Threading.Tasks;
+using WhatsApp.Services.Navigation;
+using WhatsApp.ViewModels;
+using WhatsApp.Views;
+using Xamarin.Forms;
+
+namespace CISPointOfSale.Services.Navigation
+{
+    public class NavigationService : INavigationService
+    {
+        public BaseViewModel PreviousPageViewModel
+        {
+            get
+            {
+                var mainPage = Application.Current.MainPage as CustomNavigationView;
+                var viewModel = mainPage.Navigation.NavigationStack[mainPage.Navigation.NavigationStack.Count - 2].BindingContext;
+                return viewModel as BaseViewModel;
+            }
+        }
+
+
+        public Task InitializeAsync()
+        {
+            return null;
+            //return NavigateToAsync<MainViewModel>();
+
+        }
+
+        public Task NavigateToAsync<TViewModel>() where TViewModel : BaseViewModel
+        {
+            return InternalNavigateToAsync(typeof(TViewModel), null);
+        }
+
+        public Task NavigateToAsync<TViewModel>(object parameter) where TViewModel : BaseViewModel
+        {
+            return InternalNavigateToAsync(typeof(TViewModel), parameter);
+        }
+
+        public Task RemoveLastFromBackStackAsync()
+        {
+            var mainPage = Application.Current.MainPage as CustomNavigationView;
+
+            if (mainPage != null)
+            {
+                mainPage.Navigation.RemovePage(
+                    mainPage.Navigation.NavigationStack[mainPage.Navigation.NavigationStack.Count - 2]);
+            }
+
+            return Task.FromResult(true);
+        }
+
+        public Task RemoveBackStackAsync()
+        {
+            var mainPage = Application.Current.MainPage as CustomNavigationView;
+
+            if (mainPage != null)
+            {
+                for (int i = 0; i < mainPage.Navigation.NavigationStack.Count - 1; i++)
+                {
+                    var page = mainPage.Navigation.NavigationStack[i];
+                    mainPage.Navigation.RemovePage(page);
+                }
+            }
+
+            return Task.FromResult(true);
+        }
+
+        private async Task InternalNavigateToAsync(Type viewModelType, object parameter)
+        {
+            Page page = CreatePage(viewModelType, parameter);
+
+
+            var navigationPage = Application.Current.MainPage as CustomNavigationView;
+            if (navigationPage != null)
+            {
+                await navigationPage.PushAsync(page);
+            }
+            else
+            {
+                Application.Current.MainPage = new CustomNavigationView(page);
+            }
+
+            await (page.BindingContext as BaseViewModel).InitializeAsync(parameter);
+        }
+
+        private Type GetPageTypeForViewModel(Type viewModelType)
+        {
+            var viewName = viewModelType.FullName.Replace("Model", string.Empty);
+            var viewModelAssemblyName = viewModelType.GetTypeInfo().Assembly.FullName;
+            var viewAssemblyName = string.Format(CultureInfo.InvariantCulture, "{0}, {1}", viewName, viewModelAssemblyName);
+            var viewType = Type.GetType(viewAssemblyName);
+            return viewType;
+        }
+
+        private Page CreatePage(Type viewModelType, object parameter)
+        {
+            Type pageType = GetPageTypeForViewModel(viewModelType);
+            if (pageType == null)
+            {
+                throw new Exception($"Cannot locate page type for {viewModelType}");
+            }
+
+            Page page = Activator.CreateInstance(pageType) as Page;
+            return page;
+        }
+
+
+    }
+}
