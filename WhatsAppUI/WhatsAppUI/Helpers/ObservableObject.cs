0abe7a6d0f7c8f29738a6dbbae263ab5dc550048
egbakou <egbakoukodjolaurent@gmail.com>
2019-05-03 06:02:37 +0000

    INotifyPropertyChanged implemented.

---
+++ WhatsAppUI/WhatsAppUI/Helpers/ObservableObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WhatsApp.Helpers
+{
+    /// <summary>
+    /// Observable object with INotifyPropertyChanged implemented
+    /// </summary>
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Sets the property.
+        /// </summary>
+        /// <returns><c>true</c>, if property was set, <c>false</c> otherwise.</returns>
+        /// <param name="backingStore">Backing store.</param>
+        /// <param name="value">Value.</param>
+        /// <param name="propertyName">Property name.</param>
+        /// <param name="onChanged">On changed.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        protected bool SetProperty<T>(ref T backingStore, T value,
+             [CallerMemberName]string propertyName = "",
+             Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
