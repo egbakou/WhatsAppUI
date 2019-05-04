5714f1a945a2c34c4e53cd1769bc4eda64efc2c2
egbakou <egbakoukodjolaurent@gmail.com>
2019-05-04 06:41:59 +0000

    ChatsList View feature added.

---
+++ WhatsAppUI/WhatsAppUI/ViewModels/ChatsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using WhatsApp.Models;
+
+namespace WhatsApp.ViewModels
+{
+    public class ChatsViewModel: BaseViewModel
+    {
+        
+        public ChatsViewModel()
+        {
+            Title = "CHATS";
+            Contacts = new ObservableCollection<Contact>
+                (Services.ContactService.GetAllContact());
+        }
+
+
+        private ObservableCollection<Contact> _contacts;
+        public ObservableCollection<Contact> Contacts
+        {
+            get { return _contacts;  }
+            set { SetProperty(ref _contacts, value); }
+        }
+        
+    }
+}
---
+++ WhatsAppUI/WhatsAppUI/Views/ChatsView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using FFImageLoading.Forms;
+using System;
+//using WhatsApp.PopupPages;
+using WhatsApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhatsApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ChatsView : ContentPage
+	{
+        public ChatsViewModel viewModel;
+		public ChatsView ()
+		{
+			InitializeComponent ();
+            BindingContext = viewModel = new ChatsViewModel();
+		}
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var imageSender = (CachedImage)sender;
+            //await Navigation.PushPopupAsync(new ChatProfileSelectionPopupView(imageSender.Source));
+
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("FAB Clicked!", "Congrats on creating your FAB!", "Thanks!");
+        }
+
+        private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            await DisplayAlert("Item", "Item tapped", "OK");
+        }
+    }
+}
