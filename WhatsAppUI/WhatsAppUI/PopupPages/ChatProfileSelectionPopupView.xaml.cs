6f66f252ba706807f172d11839dda8c9da3032b6
egbakou <egbakoukodjolaurent@gmail.com>
2019-05-04 06:46:20 +0000

    ChatProfile image PopupView added.

---
+++ WhatsAppUI/WhatsAppUI/PopupPages/ChatProfileSelectionPopupView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WhatsApp.PopupPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ChatProfileSelectionPopupView : PopupPage
+	{
+        private ChatProfileSelectionPopupViewModel viewModel;
+        public ChatProfileSelectionPopupView (ImageSource imageSource)
+		{
+			InitializeComponent ();
+            BindingContext = viewModel = new ChatProfileSelectionPopupViewModel(imageSource);
+		}
+	}
+}
---
+++ WhatsAppUI/WhatsAppUI/PopupPages/ChatProfileSelectionPopupViewModel.cs
@@ -0,0 +1,30 @@
+﻿using WhatsApp.ViewModels;
+using Xamarin.Forms;
+
+namespace WhatsApp.PopupPages
+{
+    public class ChatProfileSelectionPopupViewModel : BaseViewModel
+    {
+        private ImageSource _selectedProfileImage;
+        public ImageSource SelectedProfileImage
+        {
+            get { return _selectedProfileImage; }
+            set { SetProperty(ref _selectedProfileImage, value); }
+        }
+
+        private string _contact;
+        public string Contact
+        {
+            get { return _contact; }
+            set { SetProperty(ref _contact, value); }
+        }
+
+        public ChatProfileSelectionPopupViewModel(ImageSource source)
+        {
+            SelectedProfileImage = source;
+            // FileName to contact name; Example: Rita.jpg => Rita
+            var fileName = source.ToString().Remove(0, 6);
+            Contact = fileName.Remove(fileName.Length - 4, 4);
+        }
+    }
+}
